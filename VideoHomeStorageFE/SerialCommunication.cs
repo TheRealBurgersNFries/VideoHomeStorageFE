1f4c6d19a621dda6853810052288468641170a66
Shayne <shaynehemminger@live.com>
2016-10-01 06:56:50 -0400

    Begin Development of system to allow for controlling the Arduino

---
+++ VideoHomeStorageFE/SerialCommunication.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoHomeStorage.FE
+{
+    class SerialCommunication
+    {
+        private SerialPort ArduinoCommunication;
+
+        public SerialCommunication(string _portName)
+        {
+            try
+            {
+                ArduinoCommunication = new SerialPort(_portName);
+            } catch (Exception e) {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+        public void Play()
+        {
+            ArduinoCommunication.WriteLine("");
+        }
+        public void Record()
+        {
+            ArduinoCommunication.WriteLine("");
+        }
+
+        public void Stop()
+        {
+            ArduinoCommunication.WriteLine("");
+        }
+
+        public void Eject()
+        {
+            ArduinoCommunication.WriteLine("");
+        }
+    }
+}
