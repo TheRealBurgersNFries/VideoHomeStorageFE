ee58635247d4736b0f8dcd929cb4086e1f228409
Shayne <shaynehemminger@live.com>
2016-10-01 04:12:17 -0400

    Designed the User interface, and the system designed to interact with the Encoder

--- VideoHomeStorageFE/MainWindow.xaml.cs
+++ VideoHomeStorageFE/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using System.Diagnostics;
 
 namespace VideoHomeStorage.FE
 {
@@ -20,9 +24,90 @@ namespace VideoHomeStorage.FE
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int RowCount = 0;
+        private int ColumnCount = 0;
+        private bool ParityEnabled = false;
+        private string FileName = "";
+        private byte[] FileBytes = null;
+        private Bitmap OutputImage = null;
         public MainWindow()
         {
             InitializeComponent();
+            // Set the Values of the input boxesot the default values of the variables they correspond to.
+            ColumnCountTextBox.Text = RowCount.ToString();
+            RowCountTextBox.Text = ColumnCount.ToString();
+            FileLocationTextBox.Text = FileName;
+            ParityCheckBox.IsChecked = ParityEnabled;
+            // Set the functions of the increment buttons
+            IncrementColumnButton.Click += IncrementColumnButton_Click;
+            IncrementRowButton.Click += IncrementRowButton_Click;
+            // Set the functions of the decrement buttons
+            DecrementColumnButton.Click += DecrementColumnButton_Click;
+            DecrementRowButton.Click += DecrementRowButton_Click;
+            // Set the function of changing the ParityCheckBox
+            ParityCheckBox.Checked += ParityCheckBox_ValueChange;
+            ParityCheckBox.Unchecked += ParityCheckBox_ValueChange;
+            // Set the functions for the browse and submit values
+            BrowseButton.Click += BrowseButton_Click;
+            SubmitButton.Click += SubmitButton_Click;
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Actually set the variable equal to the input box for FileName
+            FileName = FileLocationTextBox.Text;
+            try {
+                // Usee the encoder to turn bytes into an image
+                FileBytes = File.ReadAllBytes(FileName);
+                OutputImage = VHSEncoder.Encode(FileBytes);
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    OutputImage.Save(saveFileDialog.FileName);
+                }
+            } catch (IOException exception) {
+                Debug.WriteLine(exception.Message);
+            }
+
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Let the user select a file using an OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileLocationTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void ParityCheckBox_ValueChange(object sender, RoutedEventArgs e)
+        {
+            ParityEnabled = (bool)ParityCheckBox.IsChecked;
+        }
+
+        private void IncrementColumnButton_Click(object sender, RoutedEventArgs e)
+        {
+            ColumnCount++;
+            ColumnCountTextBox.Text = ColumnCount.ToString();
+        }
+
+        private void IncrementRowButton_Click(object sender, RoutedEventArgs e)
+        {
+            RowCount++;
+            RowCountTextBox.Text = RowCount.ToString();
+        }
+
+        private void DecrementColumnButton_Click(object sender, RoutedEventArgs e)
+        {
+            ColumnCount--;
+            ColumnCountTextBox.Text = ColumnCount.ToString();
+        }
+
+        private void DecrementRowButton_Click(object sender, RoutedEventArgs e)
+        {
+            RowCount--;
+            RowCountTextBox.Text = RowCount.ToString();
         }
     }
 }
